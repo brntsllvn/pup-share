903723b485a5c753c1aff59228dc6a76ff236cd1
Brent J Sullivan <brntsllvn@gmail.com>
2015-08-24 03:45:18 +0000

    refactor contact_form tests

--- spec/models/contact_form_spec.rb
+++ spec/models/contact_form_spec.rb
@@ -2,49 +2,27 @@ require 'rails_helper'
 
 describe ContactForm do
 
-  # validations
+  let(:contact_form) { create(:contact_form) }
 
-  it 'is valid with name, email, company, type, comment' do
+  it 'valid' do
     contact_form = build(:contact_form)
     expect(contact_form).to be_valid
   end
 
-  it 'is invalid without name' do
-    contact_form = build(:contact_form, name: nil)
-    contact_form.valid?
-    expect(contact_form.errors[:name]).to include "can't be blank"
+  %w(name email company contact_type comment).each do |field|
+    context "when #{field} missing" do
+      it 'not valid' do
+        contact_form.update_attribute(field, nil)
+        expect(contact_form).not_to be_valid
+      end
+    end
   end
 
-  it 'is invalid without email' do
-    contact_form = build(:contact_form, email: nil)
-    contact_form.valid?
-    expect(contact_form.errors[:email]).to include "can't be blank"
+  context "when @ not in email;" do
+    it 'not valid' do
+      contact_form = build(:contact_form, email: 'brnt.brnt')
+      expect(contact_form).not_to be_valid
+    end
   end
-
-  it 'is invalid without company' do
-    contact_form = build(:contact_form, company: nil)
-    contact_form.valid?
-    expect(contact_form.errors[:company]).to include "can't be blank"
-  end
-
-  it 'is invalid without type' do
-    contact_form = build(:contact_form, contact_type: nil)
-    contact_form.valid?
-    expect(contact_form.errors[:contact_type]).to include "can't be blank"
-  end
-
-  it 'is invalid withouout comment' do
-    contact_form = build(:contact_form, comment: nil)
-    contact_form.valid?
-    expect(contact_form.errors[:comment]).to include "can't be blank"
-  end
-
-  it 'is invalid is email does not have \@' do
-    contact_form = build(:contact_form, email: 'brnt.brnt')
-    contact_form.valid?
-    expect(contact_form.errors[:email]).to include "Need a valid email address"
-  end
-
-  # methods
-
 end
+
