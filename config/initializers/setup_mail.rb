05a97cb306120bfd62ecc4f3b6dc5a207f1826b4
Brent J Sullivan <brntsllvn@gmail.com>
2015-08-27 22:54:22 +0000

    obscure sendgrid vars

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,9 +4,8 @@ ActionMailer::Base.smtp_settings = {
   address:               'smtp.sendgrid.net',
   port:                  '587',
   authentication:        :plain,
-  user_name:             'app36989134@heroku.com',
-  password:              'nrup4f8d4415',
-  domain:                'heroku.com',
+  user_name:             ENV['SENDGRID_USERNAME'],
+  password:              ENV['SENDGRID_PASSWORD'],
+  domain:                ENV['SENDGRID_DOMAIN'], 
   enable_starttls_auto:  true
   }
-  
