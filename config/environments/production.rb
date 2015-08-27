45c89ef4ec1272952734135c2ef0b6e622063d53
Brent J Sullivan <brntsllvn@gmail.com>
2015-08-27 22:40:08 +0000

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   
+  config.action_mailer.default_url_options = { host: 'pup-share.herokuapp.com' }
   
-  config.action_mailer.default_url_options = { :host => 'pup-share.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
