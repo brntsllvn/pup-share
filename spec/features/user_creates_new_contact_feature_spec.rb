e82f43887af6e92988aabe1119051170e4df48d4
Brent J Sullivan <brntsllvn@gmail.com>
2015-09-02 23:53:01 +0000

    clean up contact specs

--- spec/features/user_creates_new_contact_feature_spec.rb
+++ spec/features/user_creates_new_contact_feature_spec.rb
@@ -1,13 +1,16 @@
 require 'rails_helper'
 
-feature 'Submitting contact form' do
+feature 'Users submits contact form' do
 
-  let (:user) { create(:user, password: 'lolololol', password_confirmation: 'lolololol') }
+  let (:user) { create(:user) }
 
-  scenario 'Successfully submit contact form' do
+  background do
     visit '/'
     click_on 'Contact'
     expect(page).to have_content 'Submit a comment'
+  end
+
+  scenario 'success' do
     fill_in 'Name', with: 'Dave Wallace' 
     fill_in 'Email', with: 'SomeDude@me.com'
     fill_in 'Company', with: 'Intec LLC'
@@ -15,5 +18,54 @@ feature 'Submitting contact form' do
     fill_in 'Comment', with: 'Yo needs to fixed yore stuffs'
     click_on 'Submit'
     expect(page).to have_content 'Thanks for reaching out!'
+  end
+
+  context 'fails when' do
+    scenario 'name blank' do
+      fill_in 'Email', with: 'SomeDude@me.com'
+      fill_in 'Company', with: 'Intec LLC'
+      choose 'contact_form_contact_type_comment'
+      fill_in 'Comment', with: 'Yo needs to fixed yore stuffs'
+      click_on 'Submit'
+      expect(page).to have_content "can't be blank"
+    end
+
+    scenario 'email blank' do 
+      fill_in 'Name', with: 'Dave Wallace' 
+      fill_in 'Company', with: 'Intec LLC'
+      choose 'contact_form_contact_type_comment'
+      fill_in 'Comment', with: 'Yo needs to fixed yore stuffs'
+      click_on 'Submit'  
+      expect(page).to have_content "can't be blank"
+    end
+
+    scenario 'company blank' do
+      fill_in 'Email', with: 'SomeDude@me.com'
+      choose 'contact_form_contact_type_comment'
+      fill_in 'Comment', with: 'Yo needs to fixed yore stuffs'
+      click_on 'Submit'
+      expect(page).to have_content "can't be blank"
+    end
+
+    # scenario 'contact type blank' handled by schema default
+
+    scenario 'comment blank' do
+      fill_in 'Name', with: 'Dave Wallace' 
+      fill_in 'Email', with: 'SomeDude@me.com'
+      fill_in 'Company', with: 'Intec LLC'
+      choose 'contact_form_contact_type_comment'
+      click_on 'Submit'
+      expect(page).to have_content "can't be blank"
+    end
+
+    scenario 'email wrong format' do
+      fill_in 'Name', with: 'Dave Wallace' 
+      fill_in 'Email', with: 'SomeDude,,,me.com'
+      fill_in 'Company', with: 'Intec LLC'
+      choose 'contact_form_contact_type_comment'
+      fill_in 'Comment', with: 'Yo needs to fixed yore stuffs'
+      click_on 'Submit'
+      expect(page).to have_content 'Need a valid email address'
+    end
   end
 end
--- spec/features/user_creates_new_job_feature_spec.rb
+++ spec/features/user_creates_new_job_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'Creating new job' do
 
-  let (:user) { create(:user, password: 'lolololol', password_confirmation: 'lolololol') }
+  let (:user) { create(:user) }
 
   scenario 'not logged in' do
     visit jobs_path
@@ -24,8 +24,7 @@ feature 'Creating new job' do
     sign_in(user.email, user.password)
     # create pup
     visit user_path(user)
-    click_on 'New Pup'
-    create_pup(user)
+    create_pup('Ace', 'Lab', 'M', 4.0, '555-555-5555', true)
     expect(page).to have_content 'Find a Walker'
     # create job
     visit jobs_path
@@ -38,17 +37,5 @@ feature 'Creating new job' do
     fill_in 'Pick up location', with: 'some other place'     
     click_on 'Create Job'
     expect(page).to have_content 'Job created'
-  end
-
-  def create_pup(user)
-    visit user_path(user)
-    click_on 'New Pup'
-    fill_in 'Pup name', with: 'Ace'
-    fill_in 'Pup breed', with: 'Lab'
-    find(:css, "#pup_pup_gender_m").set(true)
-    fill_in 'Pup age', with: 3.5
-    fill_in 'Pup vet phone', with: '555-555-5555'
-    choose "pup_spayed_neutered_true"
-    click_on 'Create Pup'
   end
 end
