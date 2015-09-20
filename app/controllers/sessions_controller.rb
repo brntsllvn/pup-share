e9873114993ba89186da565a0af0d2845723cb2c
Brent J Sullivan <brntsllvn@gmail.com>
2015-09-20 18:39:49 +0000

    delete remnants of aborted paypal auth

--- app/controllers/sessions_controller.rb
+++
@@ -1,8 +0,0 @@
-class SessionsController < ApplicationController
-  def create
-    uid = request.env['omniauth.auth'].uid
-    user = User.where(uid: uid).first_or_create
-    session[:user_id] = user.id
-    redirect_to :root
-  end
-end
