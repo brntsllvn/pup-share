aa228cc3b8930ef62344227412b7591712ff9747
Brent J Sullivan <brntsllvn@gmail.com>
2015-09-29 23:12:23 +0000

    slight changes to example around js popup confirm

--- spec/features/owner_selects_walker_feature_spec.rb
+++ spec/features/owner_selects_walker_feature_spec.rb
@@ -6,7 +6,7 @@ feature 'Owner selects walker' do
   let (:walker) { create(:user) }
 
   background do
-    # owner creates a pup and walk
+    # owner creates pup and walk
     sign_in owner
     visit user_path(owner)
     create_pup('Ace', 'Lab', 'M', 4.0, '555-555-5555')
@@ -20,25 +20,24 @@ feature 'Owner selects walker' do
     click_on 'Sign Out'
   end
 
-  scenario 'success' do
+  scenario 'success' do # , :js => true 
     sign_in owner
     visit user_upcoming_walks_path(owner)
     # owner has not chosen a walker yet
     expect(page).to have_content('Nobody yet')
     # owner can see offer
     expect(page).to have_content(walker.first_name)
-
-    # TDD
     expect(page).to have_content('Accept offer') # button
-    ## owner selects walker by clicking on button "Choose me"
+    # owner selects walker by clicking button
     click_on 'Accept offer'
     ### see alert pop-up 
     ### click 'OK' on pup up
-    ### owner confirms
+    ### puts page.driver.browser.alert.text, this does not really work
+    # owner confirms
     expect(page).to have_content('Walk updated')
-    ## 'Nobody yet' disappears
+    # 'Nobody yet' disappears
     expect(page).to have_no_content('Nobody yet')
-    ## 'Accept offer' disappears
+    # 'Accept offer' disappears
     expect(page).to have_no_content('Accept offer')
   end
 end 
