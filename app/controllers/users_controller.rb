3476d2593a27eddaabba6f31690a629f5f619845
Brent J Sullivan <brntsllvn@gmail.com>
2015-09-30 23:09:39 +0000

    add end walk buttons/logic to walk view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
   end
 
   def upcoming_walks
-    walks = current_user.walks
+    walks = current_user.walks.where('begin_time > ?', Time.now) 
     offer_walks = []
     current_user.offers.each do |offer|
-      offer_walks << offer.walk
+      offer_walks << offer.walk if offer.walk.begin_time > Time.now 
     end
     @walks_plus_offers = (walks + offer_walks).uniq.sort_by{ |e| e[:begin_time] }
   end
