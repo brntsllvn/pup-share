4ff9b7dd09a619cdd511af1ca6783236f28da630
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-01 11:17:00 +0000

    AWS region config fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,8 @@ CarrierWave.configure do |config|
 
   config.aws_credentials = {
     access_key_id:     ENV['AWS_ACCESS_KEY'],
-    secret_access_key: ENV['AWS_SECRET_KEY']
-    # region:          ENV['']
+    secret_access_key: ENV['AWS_SECRET_KEY'],
+    region:            ENV['AWS_REGION']
     }
 
   if Rails.env.test?
