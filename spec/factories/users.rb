2c50ad38119cadf413b01d042c32ece1ab4a36c5
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-02 03:56:16 +0000

    update factory for user address attrs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,9 +6,6 @@ FactoryGirl.define do
     first_name 'Dave'
     last_name 'Wallace'
     sequence(:email) { |i| "#{i}_#{Faker::Internet.email}" } 
-    address "#{Faker::Address.street_address} #{Faker::Address.city}"
-    date_of_birth Faker::Date.backward(10000)
-    ssn_last_four '1234'
     phone '555-555-5555'
     emergency_phone '555-555-5555'
     provider 'linkedin'
