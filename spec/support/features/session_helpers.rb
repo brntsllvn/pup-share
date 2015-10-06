9f5fadf0bca8f45f66c525229c68276447ae0219
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-06 16:51:23 +0000

    replace ugly Linkedin button with clean design; update example helper

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -4,7 +4,7 @@ module Features
       OmniAuth.config.add_mock(:linkedin, {:uid => user.uid })
       visit '/'
       expect(page).to have_no_content 'Sign Out'
-      click_link 'nav-sign-in' # image/button: Sign in with LinkedIn
+      click_on 'Sign in with Linkedin'
       expect(page).to have_content 'Sign Out'
       expect(page).to have_content 'hi there'
       visit user_path(user)
