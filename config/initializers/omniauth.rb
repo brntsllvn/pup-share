e0eee1976203eddb9dbb7d4035e53821fd6a7c4e
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-11 21:03:21 +0000

    bug fix

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 
   if Rails.env.production?
     provider :linkedin, ENV['LINKEDIN_KEY_PROD'], ENV['LINKEDIN_SECRET_PROD']
-  elsif Rails.env.development?
+  else # dev and test
     provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
   end
 
