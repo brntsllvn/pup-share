10719398ccd1362d7340c5580c826d67bf37c966
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-11 21:00:24 +0000

    config production linkedin api

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  
-  provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
-  
+
+  if Rails.env.production?
+    provider :linkedin, ENV['LINKEDIN_KEY_PROD'], ENV['LINKEDIN_SECRET_PROD']
+  elsif Rails.env.development?
+    provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET']
+  end
+
 end
