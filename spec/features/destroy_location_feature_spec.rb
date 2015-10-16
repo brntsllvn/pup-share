bf9afea848319ca684515d65a912e2ac5a63b92d
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-16 21:26:49 +0000

    destroy examples

--- spec/features/destroy_location_feature_spec.rb
+++ spec/features/destroy_location_feature_spec.rb
@@ -1,30 +1,30 @@
 require 'rails_helper'
 
-feature 'User destroys phone number' do
+feature 'User destroys location' do
 
   let (:owner)        { create(:user) }
   let (:someone_else) { create(:user) }
 
   scenario 'success' do
     sign_in owner
-    create_phone_number
+    create_location
     expect{ 
       click_on "Profile & Pups"
       first('.btn-danger').click
-      }.to change(PhoneNumber, :count).by(-1)
-    expect(page).to have_content 'Number destroyed'
+      }.to change(Location, :count).by(-1)
+    expect(page).to have_content 'Location destroyed'
   end
 
   scenario 'failure' do
     sign_in owner
-    create_phone_number
+    create_location
     click_on 'Sign Out'
     sign_in someone_else
     # hacker can see, but not destroy
     visit user_path owner
-    expect(page).to have_content(PhoneNumber.last.number)
+    expect(page).to have_content(Location.last.full_street_address)
     expect{ 
       first('.btn-danger').click
-      }.to change(PhoneNumber, :count).by(0)
+      }.to change(Location, :count).by(0)
   end
 end
--- spec/features/destroy_pup_feature_spec.rb
+++ spec/features/destroy_pup_feature_spec.rb
@@ -7,24 +7,24 @@ feature 'User destroys phone number' do
 
   scenario 'success' do
     sign_in owner
-    create_phone_number
+    create_pup
     expect{ 
       click_on "Profile & Pups"
       first('.btn-danger').click
-      }.to change(PhoneNumber, :count).by(-1)
-    expect(page).to have_content 'Number destroyed'
+      }.to change(Pup, :count).by(-1)
+    expect(page).to have_content 'Pup destroyed'
   end
 
   scenario 'failure' do
     sign_in owner
-    create_phone_number
+    create_pup
     click_on 'Sign Out'
     sign_in someone_else
     # hacker can see, but not destroy
     visit user_path owner
-    expect(page).to have_content(PhoneNumber.last.number)
+    expect(page).to have_content(Pup.last.name)
     expect{ 
       first('.btn-danger').click
-      }.to change(PhoneNumber, :count).by(0)
+      }.to change(Pup, :count).by(0)
   end
 end
