5edfe0153a41816c61096258d48babbd0b76c361
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-31 21:25:54 +0000

    remove deprecated serve_static_assets

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ module PupShare
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
-    config.serve_static_assets = true
   end
   
 
