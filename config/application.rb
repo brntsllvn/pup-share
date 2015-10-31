ae7a903d9fbac5c98bd806cc67e0badfa39d2b4d
Brent J Sullivan <brntsllvn@gmail.com>
2015-10-31 21:19:04 +0000

    precompile assets manually

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module PupShare
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :delayed_job
+    config.serve_static_assets = true
   end
   
 
